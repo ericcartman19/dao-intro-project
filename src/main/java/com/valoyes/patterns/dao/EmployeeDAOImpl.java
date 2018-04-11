00289a2a0413263df68241dea7e6ce53779db69a
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-04-11 22:17:30 +0200

    configure data source information

--- src/main/java/com/valoyes/patterns/dao/EmployeeDAOImpl.java
+++ src/main/java/com/valoyes/patterns/dao/EmployeeDAOImpl.java
@@ -1,10 +1,20 @@
 package com.valoyes.patterns.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class EmployeeDAOImpl implements EmployeeDAO {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public void create(Employee employee) {
-		// TODO Auto-generated method stub
+
+		String sql = "insert into employee value(?, ?)";
+		jdbcTemplate.update(sql, employee.getId(), employee.getName());
 		
 	}
 
